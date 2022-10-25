bd7fb87b98b8715c208e31ca9435581608234f6f
Hownone <1371117687@qq.com>
2022-10-25 16:40:32 +0800

    建立欢迎主页面

---
+++ less.js-master/packages/less/src/less/tree/debug-info.js
@@ -0,0 +1,37 @@
+function asComment(ctx) {
+    return `/* line ${ctx.debugInfo.lineNumber}, ${ctx.debugInfo.fileName} */\n`;
+}
+
+function asMediaQuery(ctx) {
+    let filenameWithProtocol = ctx.debugInfo.fileName;
+    if (!/^[a-z]+:\/\//i.test(filenameWithProtocol)) {
+        filenameWithProtocol = `file://${filenameWithProtocol}`;
+    }
+    return `@media -sass-debug-info{filename{font-family:${filenameWithProtocol.replace(/([.:\/\\])/g, function (a) {
+        if (a == '\\') {
+            a = '\/';
+        }
+        return `\\${a}`;
+    })}}line{font-family:\\00003${ctx.debugInfo.lineNumber}}}\n`;
+}
+
+function debugInfo(context, ctx, lineSeparator) {
+    let result = '';
+    if (context.dumpLineNumbers && !context.compress) {
+        switch (context.dumpLineNumbers) {
+            case 'comments':
+                result = asComment(ctx);
+                break;
+            case 'mediaquery':
+                result = asMediaQuery(ctx);
+                break;
+            case 'all':
+                result = asComment(ctx) + (lineSeparator || '') + asMediaQuery(ctx);
+                break;
+        }
+    }
+    return result;
+}
+
+export default debugInfo;
+
